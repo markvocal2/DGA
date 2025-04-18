c019d2697d312a7c1d652fd7d3f24debc617bc32
MarkSurawit <markvocal2@gmail.com>
2025-04-18 22:18:57 +0700

    Delete wordpress/wp-content/themes/wpdevs/wpdevs directory

--- wordpress/wp-content/themes/wpdevs/wpdevs/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="colophon" class="site-footer">
-        <div class="site-info">
-            &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
